120e1e4b1f21098b5b59acda55fff5bcf98af8ba
MahmoudAdelAli <mssmahmoud4@gmail.com>
2024-12-24 23:39:47 +0200

    Lecture 7 React Js State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
-export default function Button() {
+import { sculptureList } from './data2.js';
+import {use, useState} from "react";
+
+function Gallery() {
+    // let index = 0 ;
+    const [num , setNum] = useState(0);
+    const [showMore , setShowMore] = useState(false);
+
+    function handleClick()
+    {
+        setNum(num +1);
+    }
+    function handleMoreClick() {
+        setShowMore(!showMore);
+    }
+    let sculpture = sculptureList[num];
     return (
-        <button
-            onClick={() => {alert('Hello, World!')}}
-            onMouseEnter={ () => {alert('Mouse is over the button')}}
-        >
-           Thing
-        </button>
+        <>
+            <button onClick={handleClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name} </i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({num + 1} of {sculptureList.length})
+            </h3>
+            <hr/>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} Details
+            </button>
+            {showMore && <p> {sculpture.description} </p>}
+            <hr/>
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+        </>
     );
 }
+export default function App()
+{
+    return (
+        <div>
+            <h1> Sculpture Gallery </h1>
+            <Gallery/>
+            <Gallery/>
+        </div>
+    );
+}
+/*
 
-// Declare a function called handleClick inside your Button component. [Done]
-// Implement the logic inside that function (use alert to show the message). [Done]
-// Add onClick={handleClick} to the <button> JSX.
+How does React know which state to return?
 
-//
+https://react.dev/learn/state-a-components-memory
 
-// https://react.dev/learn/understanding-your-ui-as-a-tree
-// https://www.youtube.com/
+
+Challenges =>
+
+https://react.dev/learn/state-a-components-memory
+
+Here
+https://documenter.getpostman.com/view/4858910/S1LpZrgg#700eaa92-b575-4674-8640-2f6959e9a287
+Or
+https://jsonplaceholder.typicode.com/
+
+( Name , Description )
+
+1- create panel to contain ( 1 or more input )
+2- separate the data panels ( next , next , previous )
+3- progress bar ,
+4- next or finish button
+
+https://www.w3schools.com/howto/howto_js_form_steps.asp?
+
+*
+*  */
+
+
+
